da64f3cf264617ab1872ea2dcccba8379e632d44
MIKEINTOSHSYSTEMS <mikeintoshsys@gmail.com>
2025-09-04 19:48:49 +0300

    reverted last commit for app config

--- application/config/app-config.php
+++ application/config/app-config.php
@@ -17,8 +17,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 * environments.
 *
 */
-//define('APP_BASE_URL', 'http://gymexp/dev/'); // development
-define('APP_BASE_URL', 'http://gymexp.mikeintoshsys.com/app/'); // production
+define('APP_BASE_URL', 'http://gymexp/dev/');
 
 /*
 * --------------------------------------------------------------------------
